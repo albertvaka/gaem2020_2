39d30db038b85d765bfff1cb78c0907efa674192
albertnez <albertmg7@gmail.com>
2020-04-19 23:50:04 +0200

    ♥🐶Dogs show their affection and status

--- src/doggo.h
+++ src/doggo.h
@@ -11,11 +11,13 @@
 const float kVel = 50;
 const float kMenjarDecreasePerSecond = 1;
 const float kMaxMenjar = 60;
+const float kMenjarBubbleDuration = 3.0f;
 static int doggo_ids = 0;
 
 struct Doggo : BoxEntity, EntS<Doggo>
 {
 	float menjar = kMaxMenjar;
+	float menjar_bubble_timer = 0.0f;
 	Animation anim;
 	int id = doggo_ids++;
 	int nextPoint;
@@ -55,6 +57,7 @@ struct Doggo : BoxEntity, EntS<Doggo>
 	{
 
 		anim.Update(dt * 1000);
+		menjar_bubble_timer -= dt;
 
 		if (wantFood) {
 			menjar -= kMenjarDecreasePerSecond * dt;
@@ -94,21 +97,44 @@ struct Doggo : BoxEntity, EntS<Doggo>
 #endif
 	}
 
-	void Draw(sf::RenderTarget& window) const
-	{
-		sf::Sprite& spr = Assets::doggoSprite;
-		spr.setOrigin(size/2.0f);
-		spr.setPosition(pos.x, pos.y);
-		spr.setTextureRect(anim.CurrentFrame());
-		pos.Debuggerino();
-		window.draw(spr);
-		if (Debug::Draw) {
-			bounds().Draw(window);
-		}
-	}
+  void Draw(sf::RenderTarget& window) const
+  {
+    sf::Sprite& spr = Assets::doggoSprite;
+    spr.setOrigin(size / 2.0f);
+    spr.setPosition(pos.x, pos.y);
+    spr.setTextureRect(anim.CurrentFrame());
+    pos.Debuggerino();
+    window.draw(spr);
+    if (Debug::Draw) {
+      bounds().Draw(window);
+    }
+
+    if (menjar_bubble_timer > 0.0f && int(menjar_bubble_timer) != 1) {
+      sf::Sprite bubble_sprite;
+      bubble_sprite.setTexture(Assets::plantTexture);
+      bubble_sprite.setTextureRect({16,0,18,23});
+      bubble_sprite.setPosition(vec(pos) + vec(17.0f, -15.f));
+      window.draw(bubble_sprite);
+    }
+
+		// Draw bottom status.
+    sf::Sprite status_sprite;
+    status_sprite.setTexture(Assets::plantTexture);
+    status_sprite.setTextureRect({ 52,32,17,16 });
+    status_sprite.setOrigin(17 / 2, 0);
+    status_sprite.setPosition(pos + vec(0.0f, 17.0f));
+    window.draw(status_sprite);
+    // Draw face.
+    int happiness = std::min(int(menjar / (kMaxMenjar / 5)), 4);
+    status_sprite.setTextureRect({ 106,52 - 12 * happiness,13,12 });
+    status_sprite.setOrigin(6, 0);
+    status_sprite.setPosition(pos + vec(0.0f, 20.0f));
+    window.draw(status_sprite);
+  }
 
 	void Feed() {
 		menjar = kMaxMenjar;
+		menjar_bubble_timer = kMenjarBubbleDuration;
 	}
 
 };
